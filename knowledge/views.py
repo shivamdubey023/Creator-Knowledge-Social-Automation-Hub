3edb786c8fa59e58dfd20aa1f5ec99ef9a36a5d8
kuro-shiv <dubeysk2003@gmail.com>
2026-03-06 16:38:51 +0530

    lets pause

--- knowledge/views.py
+++ knowledge/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Knowledge
+from .serializers import KnowledgeSerializers
 
 
+class KnowledgeListCreateView(generics.ListCreateAPIView):
+    queryset = Knowledge.objects.all()
+    serializer_class = KnowledgeSerializers
+
+
+class KnowledgeDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Knowledge.objects.all()
+    serializer_class = KnowledgeSerializers
